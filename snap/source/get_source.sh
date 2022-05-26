feb9bfde217fca2115c4736737863a27dbd3446c
Joffrey Bienvenu <joffreybvn@gmail.com>
2022-05-26 21:56:23 +0200

    Add script to download PBO client automatically

---
+++ snap/source/get_source.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Get the sources
+wget -O pro_client_linux.zip "https://ddl.pokemonrevolution.net/PROClient_linux.zip?Spring_22_v2"
+
+# Extract to "zip" folder
+unzip pro_client_linux.zip
+mv ./PROClient ./zip
